4f51f32fb04cbaa4a5aa2e9fea945b309dd9b1cc
moaz-141 <98176958+moaz-ze69@users.noreply.github.com>
2022-12-24 17:37:14 +0500

    some work on cart items details

--- Semester Project/Frontend/food-ordering-app/src/pages/Cart/index.jsx
+++ Semester Project/Frontend/food-ordering-app/src/pages/Cart/index.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { useSelector } from "react-redux";
 
+import useTabSwitch from "../../hooks/useTabSwitch";
 import Tabs from "../../components/Tabs";
 import Button from "../../components/elements/Button";
 import { ReactComponent as ArrowRightSvg } from "../../assets/icons/arrow-right-long-svgrepo-com.svg";
-import { selectAllProducts } from "../../stores/menu/productsSlice";
+import { cartProducts } from "../../stores/cart/cartSlice";
 
 const Cart = () => {
-  const cart = useSelector(selectAllProducts);
+  const cart = useSelector(cartProducts);
   const tabs = ["Summary", "Delivery", "Payment"];
+  const [currentTab, handleTabSwitch] = useTabSwitch(tabs, "Summary");
 
-  if (!cart || cart.products.length === 0) {
+  if (!cart || cart.length === 0) {
     return (
       <div className="bg-white h-full text-black flex justify-center p-4">
         <h1>Your Cart is Empty</h1>
@@ -19,8 +21,8 @@ const Cart = () => {
   }
 
   return (
-    <div className="text-white">
-      <h1>Cart</h1>
+    <div className="bg-white h-screen text-black mx-auto mt-2 border border-gray-200 p-4 md:w-2/3 rounded-lg shadow-md sm:p-6 lg:p-8">
+      <Tabs list={tabs} onTabSwitch={handleTabSwitch} activeTab={currentTab} />
     </div>
   );
 };
