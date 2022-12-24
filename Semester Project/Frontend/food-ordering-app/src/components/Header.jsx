79974635bdd03eca85d68a5c75d31f08b71d608f
moaz-141 <98176958+moaz-ze69@users.noreply.github.com>
2022-12-24 15:33:38 +0500

    added cart items counter on cart symbol in header

--- Semester Project/Frontend/food-ordering-app/src/components/Header.jsx
+++ Semester Project/Frontend/food-ordering-app/src/components/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import foody from "../../src/assets/images/foody.png";
 import cartIcon from "../../src/assets/icons/cart.svg";
 
-const Header = () => {
+const Header = ({ cartCount }) => {
   return (
     <nav id="header" className="bg-black text-white">
       <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
@@ -25,8 +25,13 @@ const Header = () => {
           </Link>
         </div>
         <div className="flex items-center justify-center space-x-4">
-          <Link to="/cart">
+          <Link to="/cart" className="mr-4 relative">
             <img src={cartIcon} alt="cart" />
+            {cartCount > 0 ? (
+              <div className="rounded-full bg-yellow-400 text-white inline-flex justify-center items-center w-full absolute -top-1 -right-1">
+                {cartCount}
+              </div>
+            ) : null}
           </Link>
           <Link to="/login">Log In</Link>
           <Link to="/register">Sign Up</Link>
--- Semester Project/Frontend/food-ordering-app/src/navigation/index.jsx
+++ Semester Project/Frontend/food-ordering-app/src/navigation/index.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "../components/Header";
+import { useSelector } from "react-redux";
 
+import Header from "../components/Header";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Menu from "../pages/Menu";
 import Cart from "../pages/Cart";
 import PaymentSuccess from "../pages/PaymentSuccess";
 import Register from "../pages/Register";
+import { cartProducts } from "../stores/cart/cartSlice";
 
 const Navigation = () => {
+  const cartItems = useSelector(cartProducts);
+
   return (
     <BrowserRouter>
-      <Header />
+      <Header cartCount={cartItems ? cartItems.length : 0} />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
