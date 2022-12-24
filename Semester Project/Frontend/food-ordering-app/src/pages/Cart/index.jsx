ce693add5de1dbf6bbbb8f148e14c1c90ede9088
moaz-141 <98176958+moaz-ze69@users.noreply.github.com>
2022-12-24 16:35:22 +0500

    display empty cart message

--- Semester Project/Frontend/food-ordering-app/src/pages/Cart/index.jsx
+++ Semester Project/Frontend/food-ordering-app/src/pages/Cart/index.jsx
@@ -1,6 +1,23 @@
 import React from "react";
+import { useSelector } from "react-redux";
+
+import Tabs from "../../components/Tabs";
+import Button from "../../components/elements/Button";
+import { ReactComponent as ArrowRightSvg } from "../../assets/icons/arrow-right-long-svgrepo-com.svg";
+import { selectAllProducts } from "../../stores/menu/productsSlice";
 
 const Cart = () => {
+  const cart = useSelector(selectAllProducts);
+  const tabs = ["Summary", "Delivery", "Payment"];
+
+  if (!cart || cart.products.length === 0) {
+    return (
+      <div className="bg-white h-full text-black flex justify-center p-4">
+        <h1>Your Cart is Empty</h1>
+      </div>
+    );
+  }
+
   return (
     <div className="text-white">
       <h1>Cart</h1>
