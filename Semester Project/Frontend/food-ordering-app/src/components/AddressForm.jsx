79e6dc4bf5ed26233f9fc28fbaac9c5bebc0b174
moaz-141 <98176958+moaz-ze69@users.noreply.github.com>
2022-12-25 22:10:51 +0500

    style on address, passed into state

--- Semester Project/Frontend/food-ordering-app/src/components/AddressForm.jsx
+++ Semester Project/Frontend/food-ordering-app/src/components/AddressForm.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
 
 import Button from "./elements/Button";
 import { ReactComponent as ArrowRightSvg } from "../assets/icons/arrow-right-long-svgrepo-com.svg";
+import { setAddress } from "../stores/userInfo/addressSlice";
 
-const AddressForm = () => {
-  const { register, handleSubmit } = useForm();
+const AddressForm = ({ onTabSwitch }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const dispatch = useDispatch();
 
   const onSubmit = (data) => {
-    console.log(data);
+    dispatch(setAddress(data));
+    onTabSwitch("Payment");
   };
 
   return (
@@ -25,12 +33,15 @@ const AddressForm = () => {
           Street Address
         </label>
         <input
-          {...register("address")}
-          className="w-full px-3 mb-3 text-sm leading-tight text-gray-700 border shadow appearance-none focus:outline-none focus:shadow-outline"
+          {...register("address", { required: true })}
+          className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border roundedn shadow appearance-none focus:outline-none focus:shadow-outline"
           id="street address"
           type="text"
           placeholder="Street Address"
         />
+        {errors.address && (
+          <span className="text-red-500">This field is required</span>
+        )}
       </div>
       <div className="mb-4 md:flex md:justify-between">
         <div className="mb-4 md:mr-2 md:mb-0 flex-1">
@@ -42,7 +53,7 @@ const AddressForm = () => {
           </label>
           <input
             {...register("city")}
-            className="w-full px-3 mb-3 text-sm leading-tight text-gray-700 border shadow appearance-none focus:outline-none focus:shadow-outline"
+            className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border roundedn shadow appearance-none focus:outline-none focus:shadow-outline"
             id="city"
             type="text"
             placeholder="City"
@@ -57,14 +68,13 @@ const AddressForm = () => {
           </label>
           <input
             {...register("state")}
-            className="w-full px-3 mb-3 text-sm leading-tight text-gray-700 border shadow appearance-none focus:outline-none focus:shadow-outline"
+            className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border roundedn shadow appearance-none focus:outline-none focus:shadow-outline"
             id="state"
             type="text"
-            placeholder="State"
+            placeholder="state"
           />
         </div>
       </div>
-
       <div className="mb-4 md:flex md:justify-between">
         <div className="mb-4 md:mr-2 md:mb-0 flex-1">
           <label
@@ -75,7 +85,7 @@ const AddressForm = () => {
           </label>
           <input
             {...register("country")}
-            className="w-full px-3 mb-3 text-sm leading-tight text-gray-700 border shadow appearance-none focus:outline-none focus:shadow-outline"
+            className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border roundedn shadow appearance-none focus:outline-none focus:shadow-outline"
             id="country"
             type="text"
             placeholder="Country"
@@ -90,7 +100,7 @@ const AddressForm = () => {
           </label>
           <input
             {...register("postalCode")}
-            className="w-full px-3 mb-3 text-sm leading-tight text-gray-700 border shadow appearance-none focus:outline-none focus:shadow-outline"
+            className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border roundedn shadow appearance-none focus:outline-none focus:shadow-outline"
             id="postal code"
             type="text"
             placeholder="Postal Code"
@@ -98,7 +108,7 @@ const AddressForm = () => {
         </div>
       </div>
       <div className="flex justify-end p-2">
-        <Button variant="dark" type="submit" className="flex items-center">
+        <Button variant="dark" className="flex items-center" type="submit">
           <span className="mr-1">Next</span>
           <ArrowRightSvg />
         </Button>
